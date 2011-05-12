11b64aab26a82e43705b81b133af91a3389f3d40
Filip Kinsky <filip@filovo.net>
2011-05-12 10:01:16 +0200

    changed the listener to Fabio's suggested implementation - http://fabiomaulo.blogspot.com/2011/05/nhibernate-bizarre-audit.html

--- NHListenerTest/SetModificationTimeFlushEntityEventListener.cs
+++ NHListenerTest/SetModificationTimeFlushEntityEventListener.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Linq;
 using NHibernate.Cfg;
 using NHibernate.Engine;
 using NHibernate.Event;
+using NHibernate.Intercept;
+using NHibernate.Persister.Entity;
 
 namespace NHListenerTest
 {
-	public class SetModificationTimeFlushEntityEventListener : IFlushEntityEventListener
+	public class SetModificationTimeFlushEntityEventListener : IFlushEntityEventListener, ISaveOrUpdateEventListener, IMergeEventListener
 	{
 		public SetModificationTimeFlushEntityEventListener()
 		{
@@ -15,27 +18,86 @@ namespace NHListenerTest
 
 		public Func<DateTime> CurrentDateTimeProvider { get; set; }
 
-		private void SetModificationDateIfPossible(object entity)
+		public void OnFlushEntity(FlushEntityEvent @event)
 		{
+			var entity = @event.Entity;
+			var entityEntry = @event.EntityEntry;
+
+			if (entityEntry.Status == Status.Deleted)
+			{
+				return;
+			}
 			var trackable = entity as ITrackModificationDate;
-			if (trackable != null)
+			if (trackable == null)
+			{
+				return;
+			}
+			if (HasDirtyProperties(@event))
 			{
 				trackable.LastModified = CurrentDateTimeProvider();
 			}
 		}
 
-		public void OnFlushEntity(FlushEntityEvent @event)
+		private bool HasDirtyProperties(FlushEntityEvent @event)
 		{
-			if (@event.EntityEntry.Status == Status.Deleted) return;
+			ISessionImplementor session = @event.Session;
+			EntityEntry entry = @event.EntityEntry;
+			var entity = @event.Entity;
+			if (!entry.RequiresDirtyCheck(entity) || !entry.ExistsInDatabase || entry.LoadedState == null)
+			{
+				return false;
+			}
+			IEntityPersister persister = entry.Persister;
 
-			SetModificationDateIfPossible(@event.Entity);
+			object[] currentState = persister.GetPropertyValues(entity, session.EntityMode); ;
+			object[] loadedState = entry.LoadedState;
+
+			return persister.EntityMetamodel.Properties
+				.Where((property, i) => !LazyPropertyInitializer.UnfetchedProperty.Equals(currentState[i]) && property.Type.IsDirty(loadedState[i], currentState[i], session))
+				.Any();
+		}
+
+		public void OnSaveOrUpdate(SaveOrUpdateEvent @event)
+		{
+			ExplicitUpdateCall(@event.Entity as ITrackModificationDate);
+		}
+
+		public void OnMerge(MergeEvent @event)
+		{
+			ExplicitUpdateCall(@event.Entity as ITrackModificationDate);
+		}
+
+		public void OnMerge(MergeEvent @event, IDictionary copiedAlready)
+		{
+			ExplicitUpdateCall(@event.Entity as ITrackModificationDate);
+		}
+
+		private void ExplicitUpdateCall(ITrackModificationDate trackable)
+		{
+			if (trackable == null)
+			{
+				return;
+			}
+			trackable.LastModified = CurrentDateTimeProvider();
 		}
 
 		public void Register(Configuration cfg)
 		{
 			var listeners = cfg.EventListeners;
-			listeners.FlushEntityEventListeners = new IFlushEntityEventListener[] {this}
+			listeners.FlushEntityEventListeners = new [] {this}
 				.Concat(listeners.FlushEntityEventListeners)
+				.ToArray();
+			listeners.SaveEventListeners = new[] {this}
+				.Concat(listeners.SaveEventListeners)
+				.ToArray();
+			listeners.SaveOrUpdateEventListeners = new[] {this}
+				.Concat(listeners.SaveOrUpdateEventListeners)
+				.ToArray();
+			listeners.UpdateEventListeners = new[] {this}
+				.Concat(listeners.UpdateEventListeners)
+				.ToArray();
+			listeners.MergeEventListeners = new[] {this}
+				.Concat(listeners.MergeEventListeners)
 				.ToArray();
 		}
 	}
